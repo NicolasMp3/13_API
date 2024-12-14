d521dcf4edbfae2dd6a772dc1e67226d2076af80
NicolasMp3 <164546658+NicolasMp3@users.noreply.github.com>
2024-12-14 20:42:59 +0000

    Branch

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
+import roteadorUsuario from "./routes/usuario.js";
+
+dotenv.config();
+
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
+  res.json({
+    nome: "Nicolas Morais", // Substitua pelo seu nome
+  });
+});
+
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuarios solicitada");
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
+});
+
+app.use(roteadorUsuario);
